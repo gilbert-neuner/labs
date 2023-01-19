cee2343e09a9eef612545107c1f7fc4e61d2ae92
gilbert-neuner <gneuner@g.ucla.edu>
2023-01-18 19:23:18 -0800

    fixed wordcloud function

--- Text-Analysis/wordcloud.R
+++ Text-Analysis/wordcloud.R
@@ -1,3 +1,9 @@
-wordcloud <- function(formula, data, max.words = 100, color = "black"){
-  wordcloud::wordcloud(words = data[, all.vars(formula)[2]], freq = data[, all.vars(formula)[1]], max.words = max.words, color = color)
+wordcloud <- function(formula, data, max.words = 100, color = "black") {
+  color <- eval(substitute(color), data, environment(formula))
+  if(length(color) == 1) {
+    wordcloud::wordcloud(words = data[, all.vars(formula)[2]], freq = data[, all.vars(formula)[1]], max.words = max.words, colors = color)
+  }
+  else {
+    wordcloud::wordcloud(words = data[, all.vars(formula)[2]], freq = data[, all.vars(formula)[1]], max.words = max.words, colors = color, ordered.colors = TRUE)
+  }
 }
