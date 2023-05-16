1bd7921c69be21af412008416d992e4c99dec7e2
gilbert-neuner <gneuner@g.ucla.edu>
2023-05-16 10:28:30 -0700

    added blue text

---
+++ access labs.R
@@ -0,0 +1,220 @@
+# 1a
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-1/a/lab1a.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 1b
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-1/b/lab1b.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 1c
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-1/c/lab1c.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 1d
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-1/d/lab1d.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 1e
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-1/e/lab1e.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 1f
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-1/f/lab1f.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 1g
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-1/g/lab1g.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 1h
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-1/h/lab1h.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 2a
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-2/a/lab2a.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 2b
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-2/b/lab2b.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 2c
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-2/c/lab2c.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 2d
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-2/d/lab2d.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 2e
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-2/e/lab2e.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 2f
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-2/f/lab2f.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 2g
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-2/g/lab2g.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 2h
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-2/h/lab2h.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
+
+# 2i
+
+con <- curl::curl("https://raw.githubusercontent.com/gilbert-neuner/labs/main/Unit-2/i/lab2i.html", "r")
+page <- paste(readLines(con), collapse = '\n')
+close(con)
+
+# Create a temp HTML file
+tf <- tempfile(fileext = ".html")
+writeLines(page, tf)
+
+# Display HTML file in the viewer pane.
+rstudioapi::viewer(tf)
